d088743e57cc0d0d4f663d72a7bee5e7014596de
SScott08 <68204195+SScott08@users.noreply.github.com>
2020-12-17 12:29:53 -0800

    Add files via upload

--- Plotly/Plotly/plot.js
+++ Plotly/Plotly/plot.js
@@ -1,22 +1,31 @@
-console.log(cityGrowths);
+function init() {
+  data = [{
+    x: [1, 2, 3, 4, 5],
+    y: [1, 2, 4, 8, 16] }];
+  Plotly.newPlot("plot", data);
+};
+init();
 
-var sortedCities = cityGrowths.sort((a,b) =>
-a.Increase_from_2016 - b.Increase_from_2016).reverse(); 
+d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+function updatePlotly() {
+  var dropdownMenu = d3.select("#dropdownMenu");
+  var dataset = dropdownMenu.property("value");
 
-var topFiveCities = sortedCities.slice(0,7);
-  
-var topFiveCityNames = topFiveCities.map(city => city.City);
-var topFiveCityGrowths = topFiveCities.map(city => parseInt(city.Increase_from_2016));
+  var xData = [1, 2, 3, 4, 5];
+  var yData = [];
 
-var trace = {
-    x: topFiveCityNames,
-    y: topFiveCityGrowths,
-    type: "bar"
+  if (dataset === 'dataset1') {
+    yData = [1, 2, 4, 8, 16];
   };
-  var data = [trace];
-  var layout = {
-    title: "Most Rapidly Growing Cities",
-    xaxis: { title: "City" },
-    yaxis: { title: "Population Growth, 2016-2017"}
+
+  if (dataset === 'dataset2') {
+    yData = [1, 10, 100, 1000, 10000];
   };
-  Plotly.newPlot("bar-plot", data, layout);
+
+  var trace = {
+    x: [xData],
+    y: [yData],
+  };
+
+  Plotly.restyle("plot", trace);
+};
